bdd84d75ec9ba857c8da030c0e2e7c610141cfdc
NickleDave <nickledave@users.noreply.github.com>
2018-11-09 20:58:12 -0500

    remove everything songdeck

--- src/tweetynet/cli/__init__.py
+++
@@ -1,7 +0,0 @@
-"""command-line interface functions for training,
-creating learning curves, etc."""
-
-from .make_data import make_data
-from .learncurve import learncurve
-from .summary import summary
-from .predict import predict
