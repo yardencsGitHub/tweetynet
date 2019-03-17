c1f415c4ebade1d59fd1a9e39e339b575a377f9e
NickleDave <nickledave@users.noreply.github.com>
2019-03-17 16:01:23 -0400

    bump pre-release version to 0.1.1a4 in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ URL = 'https://github.com/yardencsGitHub/tweetynet'
 EMAIL = 'yardenc@bu.edu'
 AUTHOR = 'Yarden Cohen, David Nicholson'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.1a3'
+VERSION = '0.1.1a4'
 LICENSE='BSD'
 
 REQUIRED = [
