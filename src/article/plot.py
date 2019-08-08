5e5669f01a38c25ed5c3afec9b93252a2165b66f
NickleDave <nickledave@users.noreply.github.com>
2019-08-08 15:32:50 -0400

    rewrite article.plot functions
    - add boxplot to both plots
      + helps reader focus less on "why does mean go up for two birds
        b/t 60 and 120s training data" (because noise, mostly)
        and focus more on big picture result that mean
        across birds is clearly lower as training data increases
      + also I like that this looks more like the Koumura + Okanoya paper plots
    - add title to both
      + so that ylabel in particular is more concise

--- src/article/plot.py
+++ src/article/plot.py
@@ -7,25 +7,29 @@ def frame_error_rate_test_mean(df, ax=None, figsize=(10, 7.5), save_as=None):
     if ax is None:
         fig, ax = plt.subplots()
         fig.set_size_inches(figsize)
+
+    sns.boxplot(x="train_set_dur", y="frame_error_test_mean", data=df, ax=ax)
+    for i, box in enumerate(ax.artists):
+        box.set_edgecolor('black')
+        box.set_facecolor('white')
+        box.set_linewidth(4)
+
+        # iterate over whiskers and median lines
+        for j in range(6 * i, 6 * (i + 1)):
+            ax.lines[j].set_color('black')
+            ax.lines[j].set_linewidth(4)
+
     sns.pointplot(x="train_set_dur", y="frame_error_test_mean",
                   hue="animal_ID", data=df,
                   linestyles=['--' for _ in df.animal_ID.unique()],
                   ax=ax)
 
-    mean_fet_mn = df.groupby('train_set_dur')['frame_error_test_mean'].mean()
-    with plt.rc_context({'lines.linewidth': 2}):
-        mn_plot = sns.pointplot(mean_fet_mn.index,
-                                mean_fet_mn.values,
-                                color='black', ax=ax)
-
-    ax.set_ylabel("frame error,\ntest set", fontsize=28)
+    ax.set_ylabel("frame error", fontsize=28)
     ax.set_xlabel("training set duration (s)", fontsize=28)
     ax.tick_params(labelsize=20)
     ax.legend(bbox_to_anchor=(1.01, 1), loc=2,
               borderaxespad=0., fontsize=20)
-
-    max_zorder = max([_.zorder for _ in ax.get_children()])
-    mn_plot.set_zorder(max_zorder + 1)
+    ax.set_title("mean frame error per bird on test set\nv. training set duration", fontsize=30)
 
     if save_as:
         plt.savefig(save_as)
@@ -38,25 +42,28 @@ def syllable_error_rate_test_mean(df, ax=None, figsize=(10, 7.5), save_as=None):
         fig, ax = plt.subplots()
         fig.set_size_inches(figsize)
 
+    sns.boxplot(x="train_set_dur", y="syllable_error_test_mean", data=df, ax=ax)
+    for i, box in enumerate(ax.artists):
+        box.set_edgecolor('black')
+        box.set_facecolor('white')
+        box.set_linewidth(4)
+
+        # iterate over whiskers and median lines
+        for j in range(6 * i, 6 * (i + 1)):
+            ax.lines[j].set_color('black')
+            ax.lines[j].set_linewidth(4)
+
     sns.pointplot(x="train_set_dur", y="syllable_error_test_mean",
-                hue="animal_ID", data=df,
-                linestyles=['--' for _ in df.animal_ID.unique()],
-                ax=ax)
+                  hue="animal_ID", data=df,
+                  linestyles=['--' for _ in df.animal_ID.unique()],
+                  ax=ax)
 
-    mean_sert_mn = df.groupby('train_set_dur')['syllable_error_test_mean'].mean()
-    with plt.rc_context({'lines.linewidth': 2}):
-        mn_plot = sns.pointplot(mean_sert_mn.index,
-                                mean_sert_mn.values,
-                                color='black', ax=ax)
-
-    ax.set_ylabel("syllable error rate,\ntest set", fontsize=28)
+    ax.set_ylabel("syllable error rate", fontsize=28)
     ax.set_xlabel("training set duration (s)", fontsize=28)
     ax.tick_params(labelsize=20)
     ax.legend(bbox_to_anchor=(1.01, 1), loc=2,
               borderaxespad=0., fontsize=20)
-
-    max_zorder = max([_.zorder for _ in ax.get_children()])
-    mn_plot.set_zorder(max_zorder + 1)
+    ax.set_title("mean syllable error rate per bird on test set\nv. training set duration", fontsize=30)
 
     if save_as:
         plt.savefig(save_as)
