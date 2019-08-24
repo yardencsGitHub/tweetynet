bee3b8f8f53bd40264ad90968c6b69a150642300
NickleDave <nickledave@users.noreply.github.com>
2019-08-24 16:04:56 -0400

    add tests for date_from_cbin_filename to test_article_syntax.py

--- tests/test_article_syntax.py
+++ tests/test_article_syntax.py
@@ -1,11 +1,25 @@
+from datetime import datetime
 import unittest
 
 import numpy as np
 
 import article.syntax
 
-git 
+
 class TestSyntax(unittest.TestCase):
+    def test_date_from_cbin_filename(self):
+        CBIN_FILENAME = 'bf_song_repo/gy6or6/032212/gy6or6_baseline_220312_0836.3.cbin'
+        dt_from_cbin = article.syntax.date_from_cbin_filename(CBIN_FILENAME)
+        self.assertTrue(
+            isinstance(dt_from_cbin, datetime)
+        )
+        self.assertTrue(
+            dt_from_cbin.date() == datetime(2012, 3, 22, 8, 36).date()
+        )
+        self.assertTrue(
+            dt_from_cbin.time() == datetime(2012, 3, 22, 8, 36).time()
+        )
+
     def test_find_branch_point(self):
         trans_mat = np.asarray(
             [
