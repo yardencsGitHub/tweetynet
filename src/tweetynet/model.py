db03f2911e4c7737a5d56e930bdc00fc51623cdb
David Nicholson <nickledave@users.noreply.github.com>
2020-01-22 08:31:48 -0500

    add model.py containing TweetyNetModel

---
+++ src/tweetynet/model.py
@@ -0,0 +1,20 @@
+import numpy as np
+import torch
+import vak
+
+from .network import TweetyNet
+
+
+def acc(y_pred, y):
+    return y_pred.eq(y.view_as(y_pred)).sum().item() / np.prod(y.shape)
+
+
+class TweetyNetModel(vak.Model):
+    @classmethod
+    def from_config(cls, config):
+        network = TweetyNet(**config['network'])
+        loss = torch.nn.CrossEntropyLoss(**config['loss'])
+        optimizer = torch.optim.SGD(params=network.parameters(), **config['optimizer'])
+        metrics = {'acc': vak.metrics.Accuracy(),
+                   'loss': torch.nn.CrossEntropyLoss()}
+        return cls(network=network, optimizer=optimizer, loss=loss, metrics=metrics)
