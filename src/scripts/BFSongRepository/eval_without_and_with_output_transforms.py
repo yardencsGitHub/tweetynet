cfea3e798f99c061245a7b76ff86856643b5e56d
David Nicholson <NickleDave@users.noreply.github.com>
2020-08-25 10:44:51 -0400

    update bengalese finch eval transforms script
    + add computation of the number of unlabeled errors within
      fixed number of timebins from an onset or offset
    + add docstring to metrics_df_from_toml_path
    + import functions "from" vak.labeled_timebins module,
      for brevity

--- src/scripts/BFSongRepository/eval_without_and_with_output_transforms.py
+++ src/scripts/BFSongRepository/eval_without_and_with_output_transforms.py
@@ -10,7 +10,8 @@ from tqdm import tqdm
 
 import vak.device
 import vak.files
-import vak.labeled_timebins as lbl_tb_funcs
+from vak.labeled_timebins import lbl_tb2segments, majority_vote_transform, lbl_tb_segment_inds_list, \
+    remove_short_segments
 from vak import config, io, models, transforms
 from vak.datasets.vocal_dataset import VocalDataset
 
@@ -51,6 +52,7 @@ def compute_metrics(metrics, y_true, y_pred, y_true_labels, y_pred_labels):
 
 def metrics_df_from_toml_path(toml_path,
                               min_segment_dur,
+                              n_timebin_from_onoffset,
                               device='cuda',
                               spect_key='s',
                               timebins_key='t'):
@@ -60,11 +62,22 @@ def metrics_df_from_toml_path(toml_path,
 
     Parameters
     ----------
-    toml_path
-    min_segment_dur
-    device
-    spect_key
-    timebins_key
+    toml_path : str, pathlib.Path
+        to config .toml file
+    min_segment_dur : float
+        minimum duration of a segment, in seconds.
+    n_timebin_from_onoffset : int
+        number of timebins from onset or offset in which
+        to count
+    device : str
+        device on which ``torch`` should place network.
+        Default is 'cuda'
+    spect_key : str
+        key used to access spectrogram in files of arrays.
+        Default is 's'
+    timebins_key : str
+        key used to access vector of timebins in files of arrays.
+        Default is 't'
 
     Returns
     -------
@@ -167,9 +180,9 @@ def metrics_df_from_toml_path(toml_path,
             records['spect_path'].append(spect_path)  # remove str from tuple
             y_true = y_true.to(device)
             y_true_np = np.squeeze(y_true.cpu().numpy())
-            y_true_labels, _, _ = lbl_tb_funcs.lbl_tb2segments(y_true_np,
-                                                               labelmap=labelmap,
-                                                               t=t)
+            y_true_labels, onsets_s, offsets_s = lbl_tb2segments(y_true_np,
+                                                                 labelmap=labelmap,
+                                                                 t=t)
             y_true_labels = ''.join(y_true_labels.tolist())
 
             y_pred = pred_dict[spect_path]
@@ -177,11 +190,11 @@ def metrics_df_from_toml_path(toml_path,
             y_pred = torch.flatten(y_pred)
             y_pred = y_pred.unsqueeze(0)[padding_mask]
             y_pred_np = np.squeeze(y_pred.cpu().numpy())
-            y_pred_labels, _, _ = lbl_tb_funcs.lbl_tb2segments(y_pred_np,
-                                                               labelmap=labelmap,
-                                                               t=t,
-                                                               min_segment_dur=None,
-                                                               majority_vote=False)
+            y_pred_labels, _, _ = lbl_tb2segments(y_pred_np,
+                                                  labelmap=labelmap,
+                                                  t=t,
+                                                  min_segment_dur=None,
+                                                  majority_vote=False)
             y_pred_labels = ''.join(y_pred_labels.tolist())
 
             metric_vals_batch = compute_metrics(metrics, y_true, y_pred, y_true_labels, y_pred_labels)
@@ -190,17 +203,17 @@ def metrics_df_from_toml_path(toml_path,
 
             # --- apply majority vote and min segment dur transforms separately
             # need segment_inds_list for both transforms
-            segment_inds_list = lbl_tb_funcs.lbl_tb_segment_inds_list(y_pred_np,
-                                                                      unlabeled_label=labelmap['unlabeled'])
+            segment_inds_list = lbl_tb_segment_inds_list(y_pred_np,
+                                                         unlabeled_label=labelmap['unlabeled'])
 
             # ---- majority vote transform
-            y_pred_np_mv = lbl_tb_funcs.majority_vote_transform(y_pred_np, segment_inds_list)
+            y_pred_np_mv = majority_vote_transform(y_pred_np, segment_inds_list)
             y_pred_mv = to_long_tensor(y_pred_np_mv).to(device)
-            y_pred_mv_labels, _, _ = lbl_tb_funcs.lbl_tb2segments(y_pred_np_mv,
-                                                                  labelmap=labelmap,
-                                                                  t=t,
-                                                                  min_segment_dur=None,
-                                                                  majority_vote=False)
+            y_pred_mv_labels, _, _ = lbl_tb2segments(y_pred_np_mv,
+                                                     labelmap=labelmap,
+                                                     t=t,
+                                                     min_segment_dur=None,
+                                                     majority_vote=False)
             y_pred_mv_labels = ''.join(y_pred_mv_labels.tolist())
             metric_vals_batch_mv = compute_metrics(metrics, y_true, y_pred_mv,
                                                    y_true_labels, y_pred_mv_labels)
@@ -208,17 +221,17 @@ def metrics_df_from_toml_path(toml_path,
                 records[f'{metric_name}_majority_vote'].append(metric_val)
 
             # ---- min segment dur transform
-            y_pred_np_mindur, _ = lbl_tb_funcs.remove_short_segments(y_pred_np,
-                                                                     segment_inds_list,
-                                                                     timebin_dur=timebin_dur,
-                                                                     min_segment_dur=min_segment_dur,
-                                                                     unlabeled_label=labelmap['unlabeled'])
+            y_pred_np_mindur, _ = remove_short_segments(y_pred_np,
+                                                        segment_inds_list,
+                                                        timebin_dur=timebin_dur,
+                                                        min_segment_dur=min_segment_dur,
+                                                        unlabeled_label=labelmap['unlabeled'])
             y_pred_mindur = to_long_tensor(y_pred_np_mindur).to(device)
-            y_pred_mindur_labels, _, _ = lbl_tb_funcs.lbl_tb2segments(y_pred_np_mindur,
-                                                                      labelmap=labelmap,
-                                                                      t=t,
-                                                                      min_segment_dur=None,
-                                                                      majority_vote=False)
+            y_pred_mindur_labels, _, _ = lbl_tb2segments(y_pred_np_mindur,
+                                                         labelmap=labelmap,
+                                                         t=t,
+                                                         min_segment_dur=None,
+                                                         majority_vote=False)
             y_pred_mindur_labels = ''.join(y_pred_mindur_labels.tolist())
             metric_vals_batch_mindur = compute_metrics(metrics, y_true, y_pred_mindur,
                                                        y_true_labels, y_pred_mindur_labels)
@@ -226,25 +239,55 @@ def metrics_df_from_toml_path(toml_path,
                 records[f'{metric_name}_min_segment_dur'].append(metric_val)
 
             # ---- and finally both transforms, in same order we apply for prediction
-            y_pred_np_mindur_mv, segment_inds_list = lbl_tb_funcs.remove_short_segments(y_pred_np,
-                                                                                        segment_inds_list,
-                                                                                        timebin_dur=timebin_dur,
-                                                                                        min_segment_dur=min_segment_dur,
-                                                                                        unlabeled_label=labelmap[
-                                                                                            'unlabeled'])
-            y_pred_np_mindur_mv = lbl_tb_funcs.majority_vote_transform(y_pred_np_mindur_mv,
+            y_pred_np_mindur_mv, segment_inds_list = remove_short_segments(y_pred_np,
+                                                                           segment_inds_list,
+                                                                           timebin_dur=timebin_dur,
+                                                                           min_segment_dur=min_segment_dur,
+                                                                           unlabeled_label=labelmap[
+                                                                               'unlabeled'])
+            y_pred_np_mindur_mv = majority_vote_transform(y_pred_np_mindur_mv,
                                                                      segment_inds_list)
             y_pred_mindur_mv = to_long_tensor(y_pred_np_mindur_mv).to(device)
-            y_pred_mindur_mv_labels, _, _ = lbl_tb_funcs.lbl_tb2segments(y_pred_np_mindur_mv,
-                                                                         labelmap=labelmap,
-                                                                         t=t,
-                                                                         min_segment_dur=None,
-                                                                         majority_vote=False)
+            y_pred_mindur_mv_labels, _, _ = lbl_tb2segments(y_pred_np_mindur_mv,
+                                                            labelmap=labelmap,
+                                                            t=t,
+                                                            min_segment_dur=None,
+                                                            majority_vote=False)
             y_pred_mindur_mv_labels = ''.join(y_pred_mindur_mv_labels.tolist())
             metric_vals_batch_mindur_mv = compute_metrics(metrics, y_true, y_pred_mindur_mv,
                                                           y_true_labels, y_pred_mindur_mv_labels)
             for metric_name, metric_val in metric_vals_batch_mindur_mv.items():
                 records[f'{metric_name}_min_dur_maj_vote'].append(metric_val)
+
+            # here we measure the number of times a frame error occurs involving 'unlabeled' timebins
+            # within some fixed number of timebins from an onset or offset.
+            # the idea being that if those specific frame errors account
+            # for a large number of all frame errors,
+            # then most frame errors are due to noisiness of segmentation
+            for y_pred_np_vec, y_pred_np_vec_label in zip(
+                    (y_pred_np, y_pred_np_mindur, y_pred_np_mv, y_pred_np_mindur_mv),
+                    ('y_pred_np', 'y_pred_np_mindur', 'y_pred_np_mv', 'y_pred_np_mindur_mv')
+            ):
+                frame_err_vec = y_true_np != y_pred_np_vec
+                n_frame_err = int(frame_err_vec.sum().item())
+                records[f'{y_pred_np_vec_label}_n_frame_err'].append(n_frame_err)
+                unlabeled_err = np.logical_and(
+                    frame_err_vec,
+                    np.logical_or(y_true_np == labelmap['unlabeled'], y_pred_np_vec == labelmap['unlabeled'])
+                )
+                records[f'{y_pred_np_vec_label}_n_unlabeled_frame_err'].append(
+                    int(unlabeled_err.sum().item())
+                )
+                t_unlabeled_err = t[unlabeled_err]
+                t_unlabeled_err_from_onset_offset = [
+                    min(np.abs(np.concatenate((onsets_s, offsets_s)) - a_time))
+                    for a_time in t_unlabeled_err
+                ]
+                counts, _ = np.histogram(t_unlabeled_err_from_onset_offset, bins=np.arange(0.0, 1.0, timebin_dur))
+                n_unlabeled_err_within_n_timebin = counts[:n_timebin_from_onoffset].sum()
+                records[
+                    f'{y_pred_np_vec_label}_n_unlabeled_err_within_{n_timebin_from_onoffset}_timebin_onoffset'
+                ].append(n_unlabeled_err_within_n_timebin)
 
         df = pd.DataFrame.from_records(records)
         return df
@@ -257,6 +300,9 @@ BIRD_ID_MIN_SEGMENT_DUR_MAP = {
     'gy6or6': 0.01,
     'or60yw70': 0.02,
 }
+# number of timebins from an onset or offset
+# in which we count
+N_TIMEBINS_FROM_ONOFFSET = 2
 
 
 def main():
@@ -267,7 +313,9 @@ def main():
         all_dfs = []
         for eval_toml_path in eval_toml_paths:
             print(f'computing metrics from dataset in .toml file: {eval_toml_path.name}')
-            toml_df = metrics_df_from_toml_path(eval_toml_path, min_segment_dur)
+            toml_df = metrics_df_from_toml_path(eval_toml_path,
+                                                min_segment_dur,
+                                                n_timebin_from_onoffset=N_TIMEBINS_FROM_ONOFFSET)
             date = eval_toml_path.parents[0].name  # directory name is date
             print(f'date for this .toml file: {date}')
             toml_df['date'] = date
