00364173ba51c3af7090194c8096030cc0e1de84
NickleDave <nickledave@users.noreply.github.com>
2021-03-13 18:41:12 -0500

    add to / clean up make_error_acros_birds_csv.py script
    - make script accepts optional `results_root` and `csv_filename` arguments
      + so it can be run on results from runs across all birds in
        BirdsongRecognition with different window sizes
    - remove code left over from when this was in the notebook to make a figure,
      that did not do anything relevant to creating the csv

--- src/scripts/BirdsongRecognition/make_error_across_birds_csv.py
+++ src/scripts/BirdsongRecognition/make_error_across_birds_csv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 """makes error_across_birds.csv, source data for figure that plots error across birds for Birdsong Recognition dataset"""
+from argparse import ArgumentParser
 from collections import defaultdict
 from pathlib import Path
 import re
@@ -12,24 +13,32 @@ import seaborn as sns
 
 import tqdm
 
+COLUMNS = [
+    'bird', 
+    'train_set_dur', 
+    'replicate_num', 
+    'model_name', 
+    'avg_acc', 
+    'avg_levenshtein',
+    'avg_loss',
+    'avg_segment_error_rate', 
+    'avg_error'
+]
+
 re_int = re.compile(r'[0-9]+')
 
 def int_from_dir_path(dir_path):
     name = dir_path.name
     return int(re_int.search(name)[0])
 
-REPO_ROOT = pyprojroot.here()
-BR_RESULTS_ROOT = REPO_ROOT.joinpath('results/BirdsongRecognition')
 
-ERROR_ACROSS_BIRDS_CSV_PATH = BR_RESULTS_ROOT.joinpath('error_across_birds.csv')
-
-
-def main():
-    BIRDS_ROOTS = sorted(BR_RESULTS_ROOT.glob('Bird*'))
+def main(results_root, csv_filename):
+    results_root = Path(results_root)
+    birds_roots = sorted(results_root.glob('Bird*'))
 
     dfs = []
 
-    for bird_root in BIRDS_ROOTS:
+    for bird_root in birds_roots:
         if bird_root.name == 'Bird10':
             continue  # didn't run experiment, not enough data for test set of correct duration
         else:
@@ -42,11 +51,8 @@ def main():
             dfs.append(df)
 
     curve_df = pd.concat(dfs)
-    # make 'bird' the first column
-    columns = ['bird', 'train_set_dur', 'replicate_num', 'model_name', 'avg_acc', 'avg_levenshtein', 'avg_loss', 'avg_segment_error_rate', 'avg_error']
-    curve_df = curve_df[columns]
+    curve_df = curve_df[COLUMNS]
     curve_df['bird'] = curve_df['bird'].astype('category')
-
     curve_df['avg_error'] = curve_df['avg_error'] * 100
 
     # add 'train_set_dur_ind' column that maps train set durations to consecutive integers
@@ -55,19 +61,27 @@ def main():
     # e.g., 30 and 45
     train_set_durs = sorted(curve_df['train_set_dur'].unique())
     dur_int_map = dict(zip(train_set_durs, range(len(train_set_durs))))
-    curve_df['train_set_dur_ind'] = curve_df['train_set_dur'].map(dur_int_map)
+    curve_df['train_set_dur_ind'] = curve_df['train_set_dur'].map(dur_int_map)    
+    csv_path = results_root.joinpath(csv_filename)
+    curve_df.to_csv(csv_path, index=False)
 
-    curve_df.head()
 
-    TRAIN_DUR_IND_MAP = {
-        k:v for k, v in zip(
-            sorted(curve_df['train_set_dur'].unique()), 
-            sorted(curve_df['train_set_dur_ind'].unique())
-        )
-    }
+REPO_ROOT = pyprojroot.here()
+BR_RESULTS_ROOT = REPO_ROOT.joinpath('results/BirdsongRecognition/window_size_88')
+
     
-    curve_df.to_csv(ERROR_ACROSS_BIRDS_CSV_PATH)
+def get_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--results_root', 
+                        help='root of directory containing reuslts for all birds in BirdsongRecognition',
+                        default=BR_RESULTS_ROOT)
+    parser.add_argument('--csv_filename',
+                        help='filename of .csv that will be saved by this script in results_root',
+                        default='error_across_birds.csv')
+    return parser
 
 
 if __name__ == '__main__':
-    main()
+    parser = get_parser()
+    args = parser.parse_args()
+    main(results_root=args.results_root, csv_filename=args.csv_filename)
