15c27618a946e58dc182d5415ae15ae089cc1e3c
David Nicholson <NickleDave@users.noreply.github.com>
2021-04-04 12:54:32 -0400

    ENH: add RNN parameters to __init__, fix #70
    adds a `hidden_size` parameter to `TweetyNet`,
    so we can run experiments varying the size of
    the hidden state in the RNN.
    
    - rename `num_rnn_features` to `rnn_input_size`, for clarity
      + use `rnn_input_size` in forward method
    - add `hidden_size` parameter to `TweetyNet`
      + defaults to None, in which case it is set to `rnn_input_size`.
        This maintains "legacy" behavior where we always matched the
        hidden state size to the input size.
      + use hidden size * 2 (because BiLSTM) for size of fc layer
    - add tests for `hidden_size` to tests/test_tweetynet.py
    
    also add other RNN parameters to __init__
    
      add `num_layers`, `rnn_dropout`, and `bidirectional`.
      Defaults are what we have used so far.
      Adding just in case we do need to vary these for
      follow-up experiments.
    
    did some other clean-up for TweetyNet model
    
    - make pep8 fixes in TweetyNet.__init__
    - revise comments in `forward` for clarity
    - make padding method explicit
    
      padding was working as intended, but only by accident,
      because the `TFConv2d` module always uses "SAME" padding
      unless "VALID" is specified, which could never be the
      case since we were specifying "same" (in lowercase!)
    
      - add `padding` parameter to `TweetyNet.__init__` that
        defaults to "SAME"
      - pass the argument for `padding` in to `TFConv2d` when
        creating `cnn`
      - add input validation to `TFConv2d.__init__` method
      - explicitly check whether padding is "SAME" in the
        `TFConv2d.forward` method, instead of defaulting to
        "SAME" whenever `self.padding` is not set to "VALID"
      - fix `TFConv2d` unit tests,
        use `padding='SAME'` instead of `padding_mode='same'`
    
    finally do some other clean-up in tests, needed to verify
    they actually run correctly
    
    - in `test_yarden2annot.py` fix argument `annot_file`,
      change to `annot_path`
    - remove tests/test_article_syntax.py, unused

--- src/tweetynet/network.py
+++ src/tweetynet/network.py
@@ -5,6 +5,9 @@ from torch.nn import functional as F
 
 
 class Conv2dTF(nn.Conv2d):
+
+    PADDING_METHODS = ('VALID', 'SAME')
+
     """Conv2d with padding behavior from Tensorflow
 
     adapted from
@@ -16,7 +19,15 @@ class Conv2dTF(nn.Conv2d):
     """
     def __init__(self, *args, **kwargs):
         super(Conv2dTF, self).__init__(*args, **kwargs)
-        self.padding = kwargs.get("padding", "SAME")
+        padding = kwargs.get("padding", "SAME")
+        if not isinstance(padding, str):
+            raise TypeError(f"value for 'padding' argument should be a string, one of: {self.PADDING_METHODS}")
+        padding = padding.upper()
+        if padding not in self.PADDING_METHODS:
+            raise ValueError(
+                f"value for 'padding' argument must be one of '{self.PADDING_METHODS}' but was: {padding}"
+            )
+        self.padding = padding
 
     def _compute_padding(self, input, dim):
         input_size = input.size(dim + 2)
@@ -41,26 +52,28 @@ class Conv2dTF(nn.Conv2d):
                 dilation=self.dilation,
                 groups=self.groups,
             )
-        rows_odd, padding_rows = self._compute_padding(input, dim=0)
-        cols_odd, padding_cols = self._compute_padding(input, dim=1)
-        if rows_odd or cols_odd:
-            input = F.pad(input, [0, cols_odd, 0, rows_odd])
+        elif self.padding == "SAME":
+            rows_odd, padding_rows = self._compute_padding(input, dim=0)
+            cols_odd, padding_cols = self._compute_padding(input, dim=1)
+            if rows_odd or cols_odd:
+                input = F.pad(input, [0, cols_odd, 0, rows_odd])
 
-        return F.conv2d(
-            input,
-            self.weight,
-            self.bias,
-            self.stride,
-            padding=(padding_rows // 2, padding_cols // 2),
-            dilation=self.dilation,
-            groups=self.groups,
-        )
+            return F.conv2d(
+                input,
+                self.weight,
+                self.bias,
+                self.stride,
+                padding=(padding_rows // 2, padding_cols // 2),
+                dilation=self.dilation,
+                groups=self.groups,
+            )
 
 
 class TweetyNet(nn.Module):
     def __init__(self,
                  num_classes,
                  input_shape=(1, 513, 88),
+                 padding='SAME',
                  conv1_filters=32,
                  conv1_kernel_size=(5, 5),
                  conv2_filters=64,
@@ -69,6 +82,10 @@ class TweetyNet(nn.Module):
                  pool1_stride=(8, 1),
                  pool2_size=(8, 1),
                  pool2_stride=(8, 1),
+                 hidden_size=None,
+                 rnn_dropout=0.,
+                 num_layers=1,
+                 bidirectional=True,
                  ):
         """initialize TweetyNet model
 
@@ -80,6 +97,8 @@ class TweetyNet(nn.Module):
             with 3 elements corresponding to dimensions of spectrogram windows: (channels, frequency bins, time bins).
             i.e. we assume input is a spectrogram and treat it like an image, typically with one channel,
             the rows are frequency bins, and the columns are time bins. Default is (1, 513, 88).
+        padding : str
+            type of padding to use, one of {"VALID", "SAME"}. Default is "SAME".
         conv1_filters : int
             Number of filters in first convolutional layer. Default is 32.
         conv1_kernel_size : tuple
@@ -96,6 +115,18 @@ class TweetyNet(nn.Module):
             Size of sliding window for second max pooling layer. Default is (1, 8),
         pool2_stride : two element tuple of ints
             Step size for sliding window of second max pooling layer. Default is (1, 8)
+        hidden_size : int
+            number of features in the hidden state ``h``. Default is None,
+            in which case ``hidden_size`` is set to the dimensionality of the
+            output of the convolutional neural network. This default maintains
+            the original behavior of the network.
+        rnn_dropout : float
+            If non-zero, introduces a Dropout layer on the outputs of each LSTM layer except the last layer,
+            with dropout probability equal to dropout. Default: 0
+        num_layers : int
+            Number of recurrent layers. Default is 1.
+        bidirectional : bool
+            If True, make LSTM bidirectional. Default is True.
         """
         super().__init__()
         self.num_classes = num_classes
@@ -105,15 +136,15 @@ class TweetyNet(nn.Module):
             Conv2dTF(in_channels=self.input_shape[0],
                      out_channels=conv1_filters,
                      kernel_size=conv1_kernel_size,
-                     padding='same'
+                     padding=padding
                      ),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=pool1_size,
                          stride=pool1_stride),
             Conv2dTF(in_channels=conv1_filters,
-                      out_channels=conv2_filters,
-                      kernel_size=conv2_kernel_size,
-                     padding = 'same'
+                     out_channels=conv2_filters,
+                     kernel_size=conv2_kernel_size,
+                     padding=padding,
                      ),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=pool2_size,
@@ -127,32 +158,33 @@ class TweetyNet(nn.Module):
         tmp_tensor = torch.rand(batch_shape)
         tmp_out = self.cnn(tmp_tensor)
         channels_out, freqbins_out = tmp_out.shape[1], tmp_out.shape[2]
-        self.num_rnn_features = channels_out * freqbins_out
+        self.rnn_input_size = channels_out * freqbins_out
 
-        self.rnn = nn.LSTM(input_size=self.num_rnn_features,
-                           hidden_size=self.num_rnn_features,
-                           num_layers=1,
-                           dropout=0,
-                           bidirectional=True)
+        if hidden_size is None:
+            self.hidden_size = self.rnn_input_size
+        else:
+            self.hidden_size = hidden_size
 
-        # for self.fc, in_features = num_rnn_features * 2
-        # because LSTM is bidirectional
-        # so we get features forward + features backward as output
-        self.fc = nn.Linear(self.num_rnn_features * 2, num_classes)
+        self.rnn = nn.LSTM(input_size=self.rnn_input_size,
+                           hidden_size=self.hidden_size,
+                           num_layers=num_layers,
+                           dropout=rnn_dropout,
+                           bidirectional=bidirectional)
+
+        # for self.fc, in_features = hidden_size * 2 because LSTM is bidirectional
+        # so we get hidden forward + hidden backward as output
+        self.fc = nn.Linear(in_features=self.hidden_size * 2, out_features=num_classes)
 
     def forward(self, x):
         features = self.cnn(x)
-        # stack channels so that dimension order is (batch, num_rnn_features, num time bins)
-        features = features.view(features.shape[0], self.num_rnn_features, -1)
-        # now switch dimensions for feeding to rnn,
-        # so dimension order is (num time bins, batch size, num_rnn_features)
+        # stack channels, to give tensor shape (batch, rnn_input_size, num time bins)
+        features = features.view(features.shape[0], self.rnn_input_size, -1)
+        # switch dimensions for feeding to rnn, to (num time bins, batch size, input size)
         features = features.permute(2, 0, 1)
-        rnn_output, (hidden, cell_state) = self.rnn(features)
-        # permute back to (batch, time bins, features)
-        # so we can project features down onto number of classes
+        rnn_output, _ = self.rnn(features)
+        # permute back to (batch, time bins, hidden size) to project features down onto number of classes
         rnn_output = rnn_output.permute(1, 0, 2)
         logits = self.fc(rnn_output)
-        # permute yet again
-        # so that dimension order is (batch, classes, time steps)
+        # permute yet again so that dimension order is (batch, classes, time steps)
         # because this is order that loss function expects
         return logits.permute(0, 2, 1)
--- tests/test_tweetynet.py
+++ tests/test_tweetynet.py
@@ -1,9 +1,11 @@
+import unittest
+
 import torch
 
 import tweetynet
 
 
-class TestConv2dTF:
+class TestConv2dTF(unittest.TestCase):
     def test_Conv2dTF_same_padding(self):
         input_size = (1, 513, 88)
         batch_size = tuple((10,) + input_size[:])
@@ -14,7 +16,39 @@ class TestConv2dTF:
         conv2dtf = tweetynet.network.Conv2dTF(in_channels=input_size[0],
                                               out_channels=n_filters,
                                               kernel_size=kernel_size,
-                                              padding_mode="same")
+                                              padding="SAME")
 
         out = conv2dtf(batch_of_spects)
         assert tuple(out.shape)[2:] == batch_size[2:]
+
+
+class TestHiddenSize(unittest.TestCase):
+    def test_default(self):
+        NUM_CLASSES = 11
+
+        net = tweetynet.network.TweetyNet(num_classes=NUM_CLASSES)
+
+        self.assertTrue(
+            net.rnn.input_size == net.rnn_input_size
+        )
+
+        # test that we default to hidden size equal to input size
+        self.assertTrue(
+            net.rnn.hidden_size == net.rnn_input_size
+        )
+
+    def test_not_default(self):
+        NUM_CLASSES = 11
+        HIDDEN_SIZE = 2048
+
+        net = tweetynet.network.TweetyNet(num_classes=NUM_CLASSES,
+                                          hidden_size=HIDDEN_SIZE)
+
+        self.assertTrue(
+            net.rnn.input_size == net.rnn_input_size
+        )
+
+        # test that we default to hidden size equal to input size
+        self.assertTrue(
+            net.rnn.hidden_size == net.hidden_size == HIDDEN_SIZE
+        )
