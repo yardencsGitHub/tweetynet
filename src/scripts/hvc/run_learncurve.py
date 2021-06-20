d635f96c1a071936a551113834cbccffc2e49ba4
David Nicholson <NickleDave@users.noreply.github.com>
2021-06-20 15:13:23 -0400

    WIP: first complete draft of run_learncurve script

--- src/scripts/hvc/run_learncurve.py
+++ src/scripts/hvc/run_learncurve.py
@@ -1,78 +1,245 @@
+"""re-runs a learncurve experiment, but using a support vector machine algorithm
+with hand-engineered features extracted from pre-segmented syllables
+"""
 import argparse
 import configparser
+import json
 from pathlib import Path
 
-import evfuncs
 import hvc
-import numpy as np
 import pandas as pd
 import pyprojroot
-from tqdm import tqdm
 
 import article.hvc
 
 
-def rerun_learncurve(animal_id_root,
-                     segment_params):
+def run_hvc_expt(prep_csv_path,
+                 segment_params,
+                 labelset,
+                 audio_format,
+                 results_dst,
+                 dummy_label='-',
+                 spect_params_ref='tachibana',
+                 logger=None,
+                 ):
+    """run a single ``hvc`` experiment
 
-    animal_id = animal_id_root.name
+    Parameters
+    ----------
+    prep_csv_path : str, pathlib.Path
+        to one of the .csv files saved by a run of
+        ``vak learncurve`` -- where the training set
+        is for one replicate of a specified duration,
+        sampled randomly from the total training set
+    segment_params : dict
+        of parameters to use when re-segmenting
+        the test set from ``prep_csv_path``
+    labelset : set
+        of labels, for segments in annotated vocalizations
+    audio_format : str
+        one of {'wav', 'cbin'}
+    results_dst : pathlib.Path
+        "results destination" where artifacts will be saved
+    dummy_label : str
+        dummy label given to segments when re-segmenting.
+        Default is '-'. Used because labels cannot be empty.
+    spect_params_ref : str
+        Which parameters to use for spectrograms.
+        Default is 'tachibana', that uses parameters
+        from Tachibana et al. 2014
 
-    article.hvc.resegment(prep_csv_path,
-                          seg_params,
-                          annot_dst=f'./results/hvc/annot/{animal_id}',
-                          csv_dst=f'./results/hvc/csv/{animal_id}',
-                          split='test')
+    Returns
+    -------
+    mean_segment_error_rate : float
+        the mean segment error rate
+    """
+    from vak.logging import log_or_print
 
+    # ---- make a bunch of paths right here at the top
+    # (fail early if for some reason this doesn't work)
+    annot_dst = results_dst / 'annot'
+    csv_dst = results_dst / 'csv'
+    features_dst = results_dst / 'features'
+    resegment_features_dst = results_dst / 'features' / 'resegment'
+    predict_dst = results_dst / 'predictions'
+    scores_dst = results_dst / 'scores'
+    clf_dst = results_dst / 'classifiers'
+
+    for dst in (annot_dst,
+                csv_dst,
+                features_dst,
+                resegment_features_dst,
+                predict_dst,
+                scores_dst,
+                clf_dst):
+        dst.mkdir(exist_ok=True, parents=True)
+
+    # ---- 1. blindly resegment test set
+    # (this is what would happen if dataset were not carefully hand annotated)
+    log_or_print(
+        f"(1) resegmenting test set",
+        logger=logger,
+        level="info",
+    )
+    resegment_csv_path = article.hvc.resegment.resegment(prep_csv_path,
+                                                         segment_params,
+                                                         dummy_label=dummy_label,
+                                                         annot_dst=annot_dst,
+                                                         csv_dst=csv_dst,
+                                                         split='test')
+
+    # ---- 2. extract features
+    # from (correctly segmented) set
     # also need a `spect_maker` to call `hvc.audiofileIO.make_syls`
+    log_or_print(
+        f"(2) extracting features",
+        logger=logger,
+        level="info",
+    )
     spect_params = hvc.parse.ref_spect_params.refs_dict[spect_params_ref]
     spect_params['window'] = 'Hann'
     spect_maker = hvc.audiofileIO.Spectrogram(**spect_params)
 
-    article.hvc.extract(csv_path,
-                        labelset,
-                        features_dst,
-                        csv_dst,
-                        spect_maker,
-                        audio_format,
-                        FEATURE_LIST)
+    extract_csv_path = article.hvc.extract.extract(prep_csv_path,
+                                                   labelset,
+                                                   features_dst,
+                                                   csv_dst,
+                                                   spect_maker,
+                                                   audio_format,
+                                                   article.hvc.extract.FEATURE_LIST)
 
-    article.hvc.extract(csv_path='results/hvc/csv/gy6or6/032212_prep_210423_094825.resgment.csv',
-                        labelset='-',
-                        features_dst='results/hvc/features/gy6or6/resegment/',
-                        csv_dst=csv_dst,
-                        spect_maker=spect_maker,
-                        audio_format=audio_format,
-                        feature_list=article.hvc.extract.FEATURE_LIST)
+    # extract from re-segmented test set
+    article.hvc.extract.extract(csv_path=resegment_csv_path,
+                                labelset=dummy_label,
+                                features_dst=resegment_features_dst,
+                                csv_dst=csv_dst,
+                                spect_maker=spect_maker,
+                                audio_format=audio_format,
+                                feature_list=article.hvc.extract.FEATURE_LIST)
 
-    learncurve_results_root = Path('results/Bengalese_Finches/learncurve/gy6or6/results_210509_010443/')
-    prep_csvs = vak.core.learncurve.train_dur_csv_paths._dict_from_dir(learncurve_results_root)
-    TRAINSET_DUR = 600
-    # prep_csvs = sorted(learncurve_results_root.glob('train_dur*/replicate*/*prep*csv'))
-    #### NOTICE I am using one of the duration = 600
-    learncurve_csv_path = prep_csvs[TRAINSET_DUR][0]
+    # ---- 3. train classifier
+    log_or_print(
+        f"(3) training classifiers",
+        logger=logger,
+        level="info",
+    )
+    clf = article.hvc.fit.fit(extract_csv_path,
+                              learncurve_csv_path=prep_csv_path)
 
-    clf = fit(extract_csv_path,
-              learncurve_csv_path)
+    clf_path = article.hvc.fit.save_clf(clf,
+                                        extract_csv_path,
+                                        clf_dst)
 
-    clf_dst = Path('./results/hvc/models/gy6or6')
-    save_clf(clf, clf_dst)
+    # ---- 4. get predictions
+    log_or_print(
+        f"(4) getting predictions",
+        logger=logger,
+        level="info",
+    )
+    pred_paths = article.hvc.predict.predict(extract_csv_path,
+                                             resegment_csv_path,
+                                             clf_path,
+                                             predict_dst,
+                                             labelset)
 
-    csv_root = Path('results/hvc/csv/gy6or6')
-    prep_csv_path = csv_root / '032212_prep_210423_094825.csv'
-    resegment_csv_path = csv_root / '032212_prep_210423_094825.resgment.csv'
-    labelset = 'iabcdefghjk'
+    # ---- 5. compute scores on predictions
+    log_or_print(
+        f"(5) computing segment error rates",
+        logger=logger,
+        level="info",
+    )
+    scores = article.hvc.score.segment_error_rate(prep_csv_path,
+                                                  pred_paths)
+    scores = {source: scores_tuple._asdict() for source, scores_tuple in scores.items()}
+    for source in scores:
+        scores[source]['segment_error_rates'] = scores[source]['segment_error_rates'].tolist()
+    scores_json_path = scores_dst / 'segment_error_rates.json'
+    with scores_json_path.open('w') as fp:
+        json.dump(scores, fp)
 
-    predict_dst = 'results/hvc/predictions/gy6or6'
+    return scores
 
-    pred_paths = article.hvc.predict(prep_csv_path,
-                                     resegment_csv_path,
-                                     clf_path,
-                                     predict_dst,
-                                     labelset)
 
-    scores = article.hvc.score(prep_csv_path, pred_paths, None)
+def rerun_learncurve(previous_run_path,
+                     segment_params,
+                     results_dst,
+                     logger=None):
+    from vak import config  # import here to avoid circular imports
+    from vak.core.learncurve import train_dur_csv_paths as _train_dur_csv_paths
+    from vak.logging import log_or_print
 
-    #TODO: save scores
+    previous_run_path = Path(previous_run_path)
+    toml_path = sorted(previous_run_path.glob('*.toml'))
+    assert len(toml_path) == 1, f'found more than one .toml config file: {toml_path}'
+
+    toml_path = toml_path[0]
+
+    cfg = config.parse.from_toml_path(toml_path)
+
+    # ---- get all the parameters from the config we need
+    prep_csv_path = cfg.learncurve.csv_path
+    labelset = cfg.prep.labelset
+    audio_format = cfg.prep.audio_format
+
+    # ---- make a "root" results directory for this animal id
+    animal_id = previous_run_path.parent.name
+    animal_id_results_dst = results_dst / animal_id
+    animal_id_results_dst.mkdir()
+
+    log_or_print(
+        f"Loading previous training subsets from:\n{previous_run_path}",
+        logger=logger,
+        level="info",
+    )
+
+    records = []  # used for summary results DataFrame, append once every time through loop
+
+    train_dur_csv_paths = _train_dur_csv_paths._dict_from_dir(previous_run_path)
+
+    for train_dur, csv_paths in train_dur_csv_paths.items():
+        # make train dur "root" within animal id root
+        train_dur_results_dst = animal_id_results_dst / f'train_dur_{train_dur}'
+        train_dur_results_dst.mkdir()
+        for replicate_num, this_train_dur_this_replicate_csv_path in enumerate(
+            csv_paths
+        ):
+            replicate_num += 1
+
+            # finally make replicate "dst" within train dur "root"
+            replicate_dst = train_dur_results_dst / f'replicate_{replicate_num}'
+            replicate_dst.mkdir()
+
+            this_train_dur_this_replicate_results_path = (
+                this_train_dur_this_replicate_csv_path.parent
+            )
+            log_or_print(
+                f"Training classifiers for training set duration of {train_dur},"
+                f"replicate number {replicate_num}, "
+                f"using dataset from .csv file:\n{this_train_dur_this_replicate_results_path}",
+                logger=logger,
+                level="info",
+            )
+
+            scores = run_hvc_expt(prep_csv_path=prep_csv_path,
+                                  segment_params=segment_params,
+                                  labelset=labelset,
+                                  audio_format=audio_format,
+                                  results_dst=replicate_dst,
+                                  spect_params_ref='tachibana')
+
+            for source in scores.keys():
+                mean_seg_err_rate = scores[source]['mean_segment_error_rate']
+                records.append(
+                    {
+                        'animal_id': animal_id,
+                        'train_dur': train_dur,
+                        'replicate': replicate_num,
+                        'source': source,
+                        'mean_segment_error_rate': mean_seg_err_rate,
+                    }
+                )
+    df = pd.DataFrame.from_records(records)
+    return df
 
 
 def get_segment_params(segment_params_ini, animal_id):
@@ -92,18 +259,46 @@ def get_segment_params(segment_params_ini, animal_id):
 def main(results_root,
          animal_ids,
          segment_params_ini,
-         csv_filename,
-         to_annot):
-    # import crowsetta here to avoid circular import
-    import crowsetta
+         results_dst,
+         csv_filename):
+    """re-run learncurve experiment by training Support Vector Machine classifiers
+    on engineered features extracted from segments, using the exact same splits
+    that were used when training ``tweetynet`` models
 
+    Parameters
+    ----------
+    results_root : str, pathlib.Path
+        root directory with results from running
+        ``vak learncurve`` experiments,
+        where subdirectories are individuals from dataset,
+        and each subdirectory contains results folders from a run
+        of ``vak learncurve``
+    animal_ids : list
+        of str, animal identifiers.
+        Should be names of subdirectories
+        within ``results_root``.
+        Specifies which of those subdirectories
+        will be used.
+    segment_params_ini : str
+        path to 'segment_params.ini' file that specifies parameters
+        to use when re-segmenting the test set, to benchmark
+        performance on data segmented **without** the clean up
+        that occurs during annotation by hand
+    results_dst : str, pathlib.Path
+        "results destination", where results from running this script
+        should be saved
+    """
     results_root = Path(results_root)
     animal_id_roots = sorted([results_root / animal_id for animal_id in animal_ids])
-    if not all([animal_id_root.exits() for animal_id_root in animal_id_roots]):
+    if not all([animal_id_root.exists() for animal_id_root in animal_id_roots]):
         doesnt_exist = [animal_id_root for animal_id_root in animal_id_roots if not animal_id_root.exits()]
         raise NotADirectoryError(
             f'directories for these animal IDs not found in results root:\n{doesnt_exist}'
         )
+
+    segment_params_ini = Path(segment_params_ini)
+    if not segment_params_ini.exists():
+        raise FileNotFoundError(f'segment_params_ini file not found: {segment_params_ini}')
 
     # get segment parameters, fail early if they're not found
     all_segment_params = {}
@@ -111,24 +306,36 @@ def main(results_root,
         all_segment_params[animal_id] = get_segment_params(segment_params_ini,
                                                            animal_id)
 
+    dfs = []
     for animal_id_root in animal_id_roots:
-        animal_id = animal_id_root.name
-        segment_params = all_segment_params[animal_id]
-        rerun_learncurve(animal_id_root,
-                         segment_params)
+        results_dirs = sorted(animal_id_root.glob('results_*'))
+        most_recent_results = results_dirs[-1]
 
+        print(
+            f'running experiment for animal id "{animal_id_root.name}"\n'
+            f'with results from directory: {most_recent_results.name}'
+        )
+        df = rerun_learncurve(previous_run_path=most_recent_results,
+                              segment_params=all_segment_params[animal_id_root.name],
+                              results_dst=results_dst)
+        dfs.append(df)
 
+    dfs = pd.concat(dfs)
+    hvc_summary_csv_path = results_dst / csv_filename
+    dfs.to_csv(hvc_summary_csv_path)
 
 
 PROJ_ROOT = pyprojroot.here()
 BR_RESULTS_ROOT = PROJ_ROOT / 'results' / 'Bengalese_Finches' / 'learncurve'
-SEGMENT_PARAMS_INI = PROJ_ROOT / 'data' / 'configs' / 'segment_parmas.ini'
+SEGMENT_PARAMS_INI = PROJ_ROOT / 'data' / 'configs' / 'segment_params.ini'
+DEFAULT_RESULTS_DST = PROJ_ROOT / 'results' / 'Bengalese_Finches' / 'hvc'
 
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--results_root',
-                        help=('root directory where subdirectories are individuals from dataset, '
+                        help=('root directory with results from running learncurve experiments, '
+                              ' where subdirectories are individuals from dataset, '
                               'and each subdirectory contains results folders from a run '
                               'of `vak learncurve`'),
                         default=BR_RESULTS_ROOT)
@@ -142,13 +349,11 @@ def get_parser():
                         default=SEGMENT_PARAMS_INI)
     parser.add_argument('--csv_filename',
                         help='filename of .csv that will be saved by this script in results_root',
-                        default='error_across_birds_with_cleanup.csv')
-    parser.add_argument('--to_annot',
-                        help=("if this option is added, predictions will be converted to annotations "
-                              "and then saved in an 'annotations' directory in 'results_root'. "
-                              "(If the option is not added, then 'to_annot' defaults to False.)"),
-                        action='store_true',
-                        )
+                        default='segment_error_across_birds.hvc.csv')
+    parser.add_argument('--results_dst',
+                        help=('"results destination", directory where results from '
+                              'running this script should be saved.'),
+                        default=DEFAULT_RESULTS_DST)
     return parser
 
 
@@ -159,4 +364,4 @@ if __name__ == '__main__':
          animal_ids=args.animal_ids,
          segment_params_ini=args.segment_params_ini,
          csv_filename=args.csv_filename,
-         to_annot=args.to_annot)
+         results_dst=args.results_dst)
