cdf21c36c3e46792f48d8a2bedadde689896921c
David Nicholson <nickledave@users.noreply.github.com>
2022-01-05 11:02:05 -0500

    ENH: add tweetynet/__about__.py with __version__ etc.

---
+++ src/tweetynet/__about__.py
@@ -0,0 +1,38 @@
+import os.path
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__commit__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+
+try:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+except NameError:
+    base_dir = None
+
+
+__title__ = "tweetynet"
+__summary__ = "neural network that segments and labels birdsong and other animal vocalizations"
+__uri__ = "https://github.com/yardencsGitHub/tweetynet"
+
+__version__ = "0.6.0"
+
+if base_dir is not None and os.path.exists(os.path.join(base_dir, ".commit")):
+    with open(os.path.join(base_dir, ".commit")) as fp:
+        __commit__ = fp.read().strip()
+else:
+    __commit__ = None
+
+__author__ = "Yarden Cohen, David Nicholson"
+__email__ = "yardencsgithub@users.noreply.github.com"
+
+__license__ = "BSD"
+__copyright__ = "2017-present %s" % __author__
